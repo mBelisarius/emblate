febf97cda5eb7e9b462f4388f36f92f10c248cd5
mBelisarius <95736227+mBelisarius@users.noreply.github.com>
2023-09-06 11:45:12 -0300

    Test: Sort

--- tests/Algorithms/test_sort.cpp
+++ tests/Algorithms/test_sort.cpp
@@ -51,3 +51,120 @@ TEST(HeapSortTest, SortNegatives)
         EXPECT_EQ(unsorted[i], sorted[i]);
     }
 }
+
+TEST(HeapSortTest, SortSigned)
+{
+    Emblate::Vector<int> unsorted;
+    unsorted.push_back(12);
+    unsorted.push_back(-23);
+    unsorted.push_back(20);
+    unsorted.push_back(0);
+    unsorted.push_back(-12);
+
+    Emblate::HeapSort<int> sorter(unsorted);
+    sorter.sort();
+
+    Emblate::Vector<int> sorted;
+    sorted.push_back(-23);
+    sorted.push_back(-12);
+    sorted.push_back(0);
+    sorted.push_back(12);
+    sorted.push_back(20);
+
+    for (int i = 0; i < unsorted.size(); i++)
+    {
+        EXPECT_EQ(unsorted[i], sorted[i]);
+    }
+}
+
+TEST(HeapSortTest, SortZeroStart)
+{
+    Emblate::Vector<int> unsorted;
+    unsorted.push_back(0);
+    unsorted.push_back(4);
+    unsorted.push_back(3);
+    unsorted.push_back(2);
+    unsorted.push_back(1);
+
+    Emblate::HeapSort<int> sorter(unsorted);
+    sorter.sort();
+
+    Emblate::Vector<int> sorted;
+    sorted.push_back(0);
+    sorted.push_back(1);
+    sorted.push_back(2);
+    sorted.push_back(3);
+    sorted.push_back(4);
+
+    for (int i = 0; i < unsorted.size(); i++)
+    {
+        EXPECT_EQ(unsorted[i], sorted[i]);
+    }
+}
+
+TEST(HeapSortTest, SortZeroEnd)
+{
+    Emblate::Vector<int> unsorted;
+    unsorted.push_back(4);
+    unsorted.push_back(3);
+    unsorted.push_back(2);
+    unsorted.push_back(1);
+    unsorted.push_back(0);
+
+    Emblate::HeapSort<int> sorter(unsorted);
+    sorter.sort();
+
+    Emblate::Vector<int> sorted;
+    sorted.push_back(0);
+    sorted.push_back(1);
+    sorted.push_back(2);
+    sorted.push_back(3);
+    sorted.push_back(4);
+
+    for (int i = 0; i < unsorted.size(); i++)
+    {
+        EXPECT_EQ(unsorted[i], sorted[i]);
+    }
+}
+
+TEST(HeapSortTest, SortZeroAll)
+{
+    Emblate::Vector<int> unsorted;
+    unsorted.push_back(0);
+    unsorted.push_back(0);
+    unsorted.push_back(0);
+    unsorted.push_back(0);
+    unsorted.push_back(0);
+
+    Emblate::HeapSort<int> sorter(unsorted);
+    sorter.sort();
+
+    Emblate::Vector<int> sorted;
+    sorted.push_back(0);
+    sorted.push_back(0);
+    sorted.push_back(0);
+    sorted.push_back(0);
+    sorted.push_back(0);
+
+    for (int i = 0; i < unsorted.size(); i++)
+    {
+        EXPECT_EQ(unsorted[i], sorted[i]);
+    }
+}
+
+TEST(HeapSortTest, SortOneElement)
+{
+    Emblate::Vector<int> unsorted;
+    unsorted.push_back(1);
+
+    Emblate::HeapSort<int> sorter(unsorted);
+    sorter.sort();
+
+    Emblate::Vector<int> sorted;
+    sorted.push_back(1);
+
+    for (int i = 0; i < unsorted.size(); i++)
+    {
+        EXPECT_EQ(unsorted[i], sorted[i]);
+    }
+}

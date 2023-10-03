bc7a1b873406d84426bd965bc7ed4caa19753ef5
mBelisarius <95736227+mBelisarius@users.noreply.github.com>
2023-10-03 20:02:45 -0300

    Bugfix: floor and ceiling return type

--- src/Internal/Algorithms/Math.h
+++ src/Internal/Algorithms/Math.h
@@ -25,11 +25,18 @@ namespace Emblate
      * @param val Value to compute the absolute value.
      * @return Largest integer value not greater than _val_.
      */
-    // TODO: Return type must be equal to T.
+    // TODO: Do not use type cast.
     template<typename T>
-    inline size_t floor(T val)
+    inline T floor(T val)
     {
-        return static_cast<size_t>(val);
+        auto aux = static_cast<int>(val);
+
+        if ((val < 0) && (val - aux < 0))
+        {
+            return static_cast<T>(aux - 1);
+        }
+
+        return static_cast<T>(aux);
     }
 
     /**
@@ -39,16 +46,15 @@ namespace Emblate
      * @param val Value to compute the absolute value.
      * @return Least integer value not less than _val_.
      */
-    // TODO: Return type must be equal to T.
     template<typename T>
-    inline size_t ceil(T val)
+    inline T ceil(T val)
     {
-        if ((val >= 0) && (val - static_cast<size_t>(val) > 0))
+        if (val - floor(val) > 0)
         {
-            return static_cast<size_t>(val + 1);
+            return floor(val + 1);
         }
 
-        return static_cast<size_t>(val);
+        return floor(val);
     }
 
     /**
@@ -65,7 +71,8 @@ namespace Emblate
     template<typename T>
     inline T map(T val, T from_low, T from_high, T to_low, T to_high)
     {
-        return (val - from_low) * (to_high - to_low) / (from_high - from_low) + to_low;
+        return (val - from_low) * (to_high - to_low) / (from_high - from_low)
+               + to_low;
     }
 }
 
